7d8b353ef830c21473664bc99c6d54abd97a2664
BrianLevin <bml201095@gmail.com>
2021-03-01 13:11:27 -0500

    added project description, preprocessor stream, and int main function to run the program

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// Brian's Carpet Cleaning Service.
+
+
+/* 
+    Brian's Carpet Cleaning Service
+    Charges:
+        $25 per small room
+        $35 per large room
+    Sales tax rate is 6%
+    Estimates are valid for 30 days
+
+    Prompt the user for the number of small and large rooms they would like cleaned
+    and provide an estimate such as:
+ 
+Estimate for carpet cleaning service
+Number of small rooms: 3
+Number of large rooms: 1
+Price per small room: $25
+Price per large room: $35
+Cost : $110
+Tax: $6.6
+===============================
+Total estimate: $116.6
+This estimate is valid for 30 days
+*/
+
+#include <iostream> // preprocessor stream that includes java libraries to run the program
+
+
+int main (){
+
+
+
+    return 0;
+}
